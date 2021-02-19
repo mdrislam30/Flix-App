7aad605d36c497a53851e660694b580b9bc1315f
Md Islam <moe@Mds-MacBook-Pro.fios-router.home>
2021-02-18 21:35:07 -0500

    Week 1

---
+++ Flix/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Flix
+//
+//  Created by Md Islam on 2/13/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
